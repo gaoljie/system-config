bcd54aefcab50cf7204543754f3984b7453ad42f
Jay Gao <gaoljie@gmail.com>
2020-10-27 12:43:02 -0500

    add config

--- linux_install.sh
+++ linux_install.sh
@@ -2,27 +2,24 @@
 
 # echo y | sh -c "$(wget -O- https://raw.githubusercontent.com/gaoljie/system-config/master/linux_install.sh)"
 
-# install zsh
+echo "install zsh"
 sudo apt install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o ~/.zshrc https://raw.githubusercontent.com/gaoljie/system-config/master/.zshrc
 echo "export ZSH=\"$(pwd ~)/.oh-my-zsh\"" >>  ~/.zshrc
 echo "source \$ZSH/oh-my-zsh.sh" >>  ~/.zshrc
 
-# install nodejs
+echo "install nodejs"
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# install n and yarn
-# make cache folder (if missing) and take ownership
+echo "install n yarn"
 sudo mkdir -p /usr/local/n
 sudo chown -R $(whoami) /usr/local/n
-# take ownership of node install destination folders
 sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
 npm install -g n yarn
 
-# git config
-# git configure
+echo "config git and ssh"
 if [ -f "~/.ssh/id_rsa.pub" ]; then
     echo "git already set up"
 else
@@ -32,5 +29,4 @@ else
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
     curl https://raw.githubusercontent.com/gaoljie/system-config/master/.gitconfig -o ~/.gitconfig
-
 fi
