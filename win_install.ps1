2e65de4323ba2a7a50455c8e27e08d0a096013bf
Jay Gao <gaoljie@gmail.com>
2020-10-27 15:15:31 -0500

    add win config

--- win_install.ps1
+++ win_install.ps1
@@ -3,7 +3,12 @@
 
 $env:SCOOP='D:\Applications\Scoop'
 [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
+
+$env:SCOOP_GLOBAL='D:\Applications'
+[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
+
 # run the installer
 iwr -useb get.scoop.sh | iex
 scoop bucket add extras
-scoop install dismplusplus
+
+scoop install dismplusplus 
