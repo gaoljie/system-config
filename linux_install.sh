a89682078ae86f5d897a04bf566ac4fb7281526e
Jay Gao <gaoljie@gmail.com>
2020-10-27 11:15:11 -0500

    add linux script

--- linux_install.sh
+++ linux_install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# echo y | sudo sh -c "$(wget -O- https://raw.githubusercontent.com/gaoljie/system-config/master/linux_install.sh)"
+
+# install zsh
 apt install zsh
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
