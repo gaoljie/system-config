59ba91440de597311f7903a25a44cdbed79024f7
Jay Gao <gaoljie@gmail.com>
2020-10-27 11:20:44 -0500

    add nodejs

--- linux_install.sh
+++ linux_install.sh
@@ -6,3 +6,6 @@
 apt install zsh
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt-get install -y nodejs
