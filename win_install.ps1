23f515f73eb6c4750b2cfc18cdfccfa5535999f9
Jay Gao <gaoljie@gmail.com>
2020-10-29 10:39:45 -0500

    add win config

--- win_install.ps1
+++ win_install.ps1
@@ -6,19 +6,17 @@ dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux 
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 wsl --set-default-version 2
 
-# $env:SCOOP='D:\Applications\Scoop'
-# [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
-#
-# $env:SCOOP_GLOBAL='D:\Applications'
-# [Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
-#
+$env:SCOOP='D:\Applications\Scoop'
+[Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
+
+$env:SCOOP_GLOBAL='D:\Applications'
+[Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', $env:SCOOP_GLOBAL, 'Machine')
+
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
-# run the installer
 iwr -useb get.scoop.sh | iex
 
 scoop install git
 scoop bucket add extras
 
-# scoop install dismplusplus
 scoop install webstorm insomnia notion slack vscode webtorrent potplayer quicklook snipaste autohotkey
