9504240ef6ba0bbda6517fdb782a27d7486516f3
Jay Gao <gaoljie@gmail.com>
2020-10-28 20:35:25 -0500

    add github

--- github.sh
+++ github.sh
@@ -6,7 +6,6 @@ cd Github
 mkdir yumi
 mkdir gaoljie
 cd gaoljie
-CNTX=users; NAME=gaoljie; PAGE=1
 curl "https://api.github.com/users/gaoljie/repos?per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
 cd ../yumi
 curl "https://api.github.com/orgs/helloyumi/repos?access_token=$GITHUB_API_TOKEN&per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
