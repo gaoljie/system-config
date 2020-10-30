433ccfc42aab95c7509b5789c888d1f95ab992fe
Jay Gao <gaoljie@gmail.com>
2020-10-29 18:46:08 -0700

    add github sh

--- github.sh
+++ github.sh
@@ -1,12 +1,12 @@
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/gaoljie/system-config/master/github.sh)"
 read GITHUB_API_TOKEN
-cd /mnt/d
-mkdir Git
-cd Git
-mkdir gaoljie
-cd gaoljie
-curl "https://api.github.com/users/gaoljie/repos?access_token=$GITHUB_API_TOKEN&per_page=1000&per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
-#cd /mnt/c/Users/jay
-#mkdir yumi
-#cd yumi
-#curl "https://api.github.com/orgs/helloyumi/repos?access_token=$GITHUB_API_TOKEN&per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
+#cd /mnt/d
+#mkdir Git
+#cd Git
+#mkdir gaoljie
+#cd gaoljie
+#curl "https://api.github.com/users/gaoljie/repos?access_token=$GITHUB_API_TOKEN&per_page=1000&per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
+cd /mnt/d/git
+mkdir yumi
+cd yumi
+curl "https://api.github.com/orgs/helloyumi/repos?access_token=$GITHUB_API_TOKEN&per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
